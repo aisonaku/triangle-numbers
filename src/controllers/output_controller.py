3e8467822b72f0336307fb3e9df13d38b8ace92d
Oksana Kuznetsova <kuznetsova@tutu.ru>
2020-04-17 22:20:26 +0300

    results dir added

--- src/controllers/output_controller.py
+++ src/controllers/output_controller.py
@@ -1,17 +1,26 @@
+import os
 from models.file import File
 
 class OutputController:
+    def __init__(self, results_dir="results"):
+        self.results_dir = results_dir
+
     def output_result(self, num, divisors, option, additional_data):
+        self._create_dir_if_not_exists(self.results_dir)
         if option == 1:
             result_string = self._construct_string(num, divisors)
             print(result_string)
-            self.print_to_file(result_string, "Divisors and sum of %sth term.txt" % str(additional_data))
+            self.print_to_file(result_string, "%s/Divisors and sum of %sth term.txt" % (self.results_dir, str(additional_data)))
         elif option == 2:
             result_string = self._construct_string(num, divisors)
             print("The triangle number is %s and divisors are %s" % (num, ', '.join(map(str, divisors))))
-            self.print_to_file(result_string, "The first triangle number %s.txt" % str(additional_data))
+            self.print_to_file(result_string, "%s/The first triangle number %s.txt" % (self.results_dir, str(additional_data)))
         else:
             print("Can't format output for option %s\n" % str(option))
+
+    def _create_dir_if_not_exists(self, path):
+        if not os.path.exists(path):
+            os.makedirs(path)
 
     def print_to_file(self, string, filename):
         file = File(filename)
--- src/views/menu.py
+++ src/views/menu.py
@@ -8,7 +8,7 @@ class Menu:
         self.screen = Screen()
 
     def initialize(self):
-        menu = ConsoleMenu("Main Menu", "", self.screen, show_exit_option=False)
+        menu = ConsoleMenu("Main Menu", "", self.screen)
         first_option = FunctionItem("Show all divisors for the triangle number ", self.first_option_action, menu=menu)
         second_option = FunctionItem("Find the first triangle number with over N number of divisors", self.second_option_action, menu=menu)
 
@@ -19,19 +19,29 @@ class Menu:
         # Show the menu
         menu.start()
         menu.join()
+        self.screen.flush()
 
     def first_option_action(self):
-        n = int(self.screen.input("Give the ordinal of triangle number?\n"))
-        if n < 1:
-            print("Ordinal should be a natural number\n")
-            return
-        controller = TriangleNumbersActionsController()
-        controller.print_all_nth_number_divisors(n)
+        try:
+            n = int(self.screen.input("Give the ordinal of triangle number?\n"))
+            if n >= 1:
+                controller = TriangleNumbersActionsController()
+                controller.print_all_nth_number_divisors(n)
+            else:
+                self.screen.println("Ordinal should be a natural number")
+        except ValueError:
+            self.screen.println("Natural number is expected")
+        self.screen.input("Press [Enter] to continue")
 
     def second_option_action(self):
-        n = int(self.screen.input("Give minimum number of divisors triangle number should have?\n"))
-        if n < 2:
-            print("There are no triangle numbers with less than 2 divisors\n")
-            return
-        controller = TriangleNumbersActionsController()
-        controller.print_num_with_divisors_count(n)
+        try:
+            n = int(self.screen.input("Give minimum number of divisors triangle number should have?\n"))
+            if n >= 2:
+                controller = TriangleNumbersActionsController()
+                controller.print_num_with_divisors_count(n)
+            else:
+                self.screen.println("There are no triangle numbers with less than 2 divisors")
+        except ValueError:
+            self.screen.println("Natural number is expected")
+        self.screen.input("Press [Enter] to continue")
+
